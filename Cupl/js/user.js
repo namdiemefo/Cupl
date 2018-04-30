71b1fb80f24b8969e49b4c918309c0ce656711c0
namdiemefo <32518147+namdiemefo@users.noreply.github.com>
2018-04-30 12:10:25 +0100

    Delete user.js

--- Cupl/js/user.js
+++
@@ -1,8 +0,0 @@
-function opennav() {
-    document.getElementById('sidenav').style.width = "250px";
-    document.getElementById('side').style.width = "0";
-}
-
-function closenav() {
-    document.getElementById('sidenav').style.width = "0";
-}
