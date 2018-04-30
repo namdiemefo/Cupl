9f7ce76658f06ca4854a0e81cf47540075be8ca0
namdiemefo <32518147+namdiemefo@users.noreply.github.com>
2018-04-30 12:05:06 +0100

    Add files via upload

---
+++ Cupl/js/user.js
@@ -0,0 +1,8 @@
+function opennav() {
+    document.getElementById('sidenav').style.width = "250px";
+    document.getElementById('side').style.width = "0";
+}
+
+function closenav() {
+    document.getElementById('sidenav').style.width = "0";
+}
